1285fa3125394b71d2b336863e6da31ccf6e8cfa
David Sundland <info@districtconcerts.com>
2019-03-28 22:12:01 -0400

    skip non-music events; scrapes for time, date, price, and ticketweb added

--- mrhenrysscraper.py
+++ mrhenrysscraper.py
@@ -37,9 +37,9 @@ class Scraped: # move to scraperLibraryOOP when scraper completed and tested
         return str(self.__class__) + ": " + str(self.__dict__)
 
 #Note - at present, 1 object is created, and it is mutated for every event. While this will work (and fits with older, non-OOP scrapers), a better, more OOPy approach may be to have 1 array of objects or 1 object containing separate objects for each event...
-mrHenrys = Scraped("", "Jazz&Blues", "", "", " ", "", "", "", "", "http://www.mrhenrysdc.com/", "Mr. Henry's", "https://goo.gl/maps/D8WAijc1bi92", "601 Pennsylvania Ave. SE, Washington, DC 20007", "", "", "", "", "")
+mrHenrys = Scraped("", "Jazz&Blues", "", "", " ", "", [], "", "", "http://www.mrhenrysdc.com/", "Mr. Henry's", "https://goo.gl/maps/D8WAijc1bi92", "601 Pennsylvania Ave. SE, Washington, DC 20007", "", "", "", "", "")
 
-usedLinksFile = '../scraped/usedlinks-mrhenry.csv'
+usedLinksFile = '../scraped/usedlinks-mrhenrys.csv'
 dateFormat = '%Y-%m-%d'
 numDays = 30
 linkCheckUrl = ''
@@ -47,81 +47,51 @@ linkCheckUrl = ''
 [today, pages, pageanddate] = scraperLibrary.previousScrape(usedLinksFile, dateFormat, numDays, linkCheckUrl)
 UTFcounter = 0 # Counter for number of encoding problems
 
-fileName = '../scraped/scraped-winery.csv'
-backupFileName = '../scraped/BackupFiles/WineryScraped'
+fileName = '../scraped/scraped-mrhenrys.csv'
+backupFileName = '../scraped/BackupFiles/MrHenrysScraped'
 [writer, backupwriter,datetoday,csvFile,backupCSV] = scraperLibrary.startCsvs(today,fileName,backupFileName)
 
 
-# Scraping 1 month at a time, creating URL to match: https://citywinery.com/washingtondc/tickets.html?view=calendar&month=6&year=2018
-for monthrange in range(0,2):  # look at this month & next; possibly look farther in future
-    month = ((today+relativedelta(months=+monthrange)).strftime("%m")).lstrip("0")
-    monthurl = "http://www.mrhenrysdc.com/calendar/" + (today+relativedelta(months=+monthrange)).strftime("%Y") + "-" + month.zfill(2) + "/"
+# Scraping 1 month at a time, creating URL to match: http://www.mrhenrysdc.com/calendar/2019-05/
+for monthrange in range(0,1):  # look at this month & next; possibly look farther in future
+    month = ((today+relativedelta(months=+monthrange)).strftime("%m"))
+    monthurl = "http://www.mrhenrysdc.com/calendar/" + (today+relativedelta(months=+monthrange)).strftime("%Y") + "-" + month + "/"
     html = urlopen(monthurl)
     bsObj = BeautifulSoup(html)
     for link in bsObj.findAll("a", {"class":"url"}):
         newPage = link.attrs["href"] #extract the links
         if newPage not in pages: #A new link has been found
             pages.add(newPage)
-            newhtml = newPage # An extra line of code; used 'cuz in some cases a site's base URL needs to be added to the internal link
-            html = urlopen(newhtml)
+            if "lipstick" in newPage or "boardgame" in newPage or "cheers-capitol" in newPage: #Recurring non-music events
+                continue
+            mrHenrys.newhtml = newPage
+            html = urlopen(mrHenrys.newhtml)
             print(newPage)
             bsObj = BeautifulSoup(html)
-            try:
-                iframes = bsObj.findAll("iframe") # If there's a video, grab it and toss it into the "buy music" column.  BUT - skip iframes that don't contain youtubes
-                for onei in iframes:  
-                    if "soundcloud" in onei.attrs["src"]:
-                        mrHenrys.musicurl = onei.attrs["src"]
-                        break  # If Soundcloud link found, snag it and quit
-                    else:
-                        if "youtube" in onei.attrs["src"]:
-                            mrHenrys.musicurl = onei.attrs["src"]
-                            break  # Grab first video that comes along
-                        else:
-                            mrHenrys.musicurl = ""  # In case there are iframes, but no videos
-            except:
-                mrHenrys.musicurl = ""
+            mrHenrys.date = bsObj.find("abbr", {"class":"tribe-events-start-date"}).attrs["title"] #date in YYYY-MM-DD format
+            pageanddate.add((newPage,mrHenrys.date,datetoday))  # Add link to list, paired with event date and today's date
             mrHenrys.artist = bsObj.find("h1", {"class":"tribe-events-single-event-title"}).get_text().strip()
-            date = re.findall("[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{1,2}",artistlong)[0]
             localList = scraperLibrary.getLocalList()
-            if scraperLibrary.compactWord(artist) in localList:
-                local = "Yes"
+            if scraperLibrary.compactWord(mrHenrys.artist) in localList:
+                mrHenrys.local = "Yes"
             else:
-                local = ""
+                mrHenrys.local = ""
 
-            try:
-                artistweb = bsObj.find("li", {"class":"website"}).find("a").attrs["href"]
-            except:
-                try:
-                    artistweb = bsObj.find("li", {"class":"facebook"}).find("a").attrs["href"]
-                except:
-                    artistweb = ""
-            pageanddate.add((newPage,date,datetoday))  # Add link to list, paired with event date and today's date
-            longtime = bsObj.find("span", {"class":"event-date"}).get_text().strip()
-            try:
-                starttime = re.findall("([0-9]{1,2}\:[0-6][0-9]\s*[aApP][mM])\s*[sS][tT][aA][rR][tT]",longtime)[0]
-            except:
-                try:
-                    starttime = re.findall("([0-9]{1,2}\s*[aApP][mM])\s*[sS][tT][aA][rR][tT]",longtime)[0]
-                except:
-                    print("Found no start time for above page, skipping.  Event name:", artist)
-                    continue
-            prices = bsObj.findAll("span", {"class":"price"})
-            if len(prices) == 0:
-                print("Found no price for above link, so skipping it.  Event name:", artist, "Event date:", date)
-                continue  # Events with no price are non-music events (always? almost always?)
+            longtime = bsObj.find("span", {"class":"tribe-event-date-start"}).get_text().strip()
+            starttime = re.findall("[0-9]{1,2}\:[0-5][0-9]",longtime)[0]
+            if starttime == "6:00 pm":  # events with 6:00 start time actually have 6:00 doors but 2 different event times
+                mrHenrys.starttime.extend("7:30 pm", "9:45 pm")
             else:
-                maxprice=0
-                minprice=10000
-                for oneprice in prices:
-                    oneticket = int(re.findall("([0-9]{1,2})\.[0-9]{2}",oneprice.get_text())[0])
-                    if (oneticket < minprice):
-                        minprice = oneticket
-                    if (oneticket > maxprice):
-                        maxprice = oneticket
-                if (maxprice == minprice):
-                    price = maxprice
-                else:
-                    price = "$" + str(minprice) + " to $" + str(maxprice)
+                mrHenrys.starttime.append(starttime)
+            try: # Most events are free, but 1 or 2 events a month require tickets
+                mrHenrys.price = bsObj.find("span", {"class":"tribe-events-cost"}).get_text().strip()
+                links = bsObj.findall("a")
+                for link in links:
+                    if "brownpaper" in link or "bpt" in link:
+                        mrHenrys.ticketweb = link.attrs["href"]
+                        break
+            except:
+                mrHenrys.price = 0
             descriptionWad = bsObj.find("div", {"class":"value"})
             descriptionParagraphs = descriptionWad.findAll("p")
             description = ""
@@ -146,22 +116,23 @@ for monthrange in range(0,2):  # look at this month & next; possibly look farthe
                 artistpic = bsObj.find("p", {"class":"product-image"}).find("img").attrs["src"]
 
             ticketweb = newhtml
-            write1 = (date, genre, artistpic, local, doors, price, starttime, newhtml, artist, venuelink, venuename, addressurl, venueaddress, description, readmore, musicurl, ticketweb)
-            write2 = (date, genre, artistpic, local, doors, price, starttime, newhtml, artist, venuelink, venuename, addressurl, venueaddress, description.encode('UTF-8'), readmore, musicurl, ticketweb)
-            write3 = (date, genre, artistpic, local, doors, price, starttime, newhtml, artist.encode('UTF-8'), venuelink, venuename, addressurl, venueaddress, description.encode('UTF-8'), readmore, musicurl, ticketweb)
-            try:  # Might crash with weird characters.
-                writer.writerow(write1)
-                backupwriter.writerow(write1)
-            except:
-                UTFcounter += 1
-                try:
-                    writer.writerow(write2)
-                    backupwriter.writerow(write2)
-                    print("Using UTF encoding for description", date)
+            for onetime in mrHenrys.starttime:
+                write1 = (mrHenrys.date, mrHenrys.genre, mrHenrys.artistpic, mrHenrys.local, mrHenrys.doors, mrHenrys.price, onetime, mrHenrys.newhtml, mrHenrys.artist, mrHenrys.venuelink, mrHenrys.venuename, mrHenrys.addressurl, mrHenrys.venueaddress, mrHenrys.description, mrHenrys.readmore, mrHenrys.musicurl, mrHenrys.ticketweb)
+                write2 = (mrHenrys.date, mrHenrys.genre, mrHenrys.artistpic, mrHenrys.local, mrHenrys.doors, mrHenrys.price, onetime, mrHenrys.newhtml, mrHenrys.artist, mrHenrys.venuelink, mrHenrys.venuename, mrHenrys.addressurl, mrHenrys.venueaddress, mrHenrys.description.encode('UTF-8'), mrHenrys.readmore, mrHenrys.musicurl, mrHenrys.ticketweb)
+                write3 = (mrHenrys.date, mrHenrys.genre, mrHenrys.artistpic, mrHenrys.local, mrHenrys.doors, mrHenrys.price, onetime, mrHenrys.newhtml, mrHenrys.artist.encode('UTF-8'), mrHenrys.venuelink, mrHenrys.venuename, mrHenrys.addressurl, mrHenrys.venueaddress, mrHenrys.description.encode('UTF-8'), mrHenrys.readmore, mrHenrys.musicurl, mrHenrys.ticketweb)
+                try:  # Might crash with weird characters.
+                    writer.writerow(write1)
+                    backupwriter.writerow(write1)
                 except:
-                    writer.writerow(write3)
-                    backupwriter.writerow(write3)
-                    print("Using UTF encoding for artist and description", date)
+                    UTFcounter += 1
+                    try:
+                        writer.writerow(write2)
+                        backupwriter.writerow(write2)
+                        print("Using UTF encoding for description", date)
+                    except:
+                        writer.writerow(write3)
+                        backupwriter.writerow(write3)
+                        print("Using UTF encoding for artist and description", date)
 csvFile.close()
 backupCSV.close()
 
